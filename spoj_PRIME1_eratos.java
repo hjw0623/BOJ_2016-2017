721307698629907457824102e4cab5c80c15f48f
hjw0623 <hwj0623@naver.com>
2017-03-21 15:09:49 +0900

    소수판별법 에라토스테네스 체 활용

---
+++ spoj_PRIME1_eratos.java
@@ -0,0 +1,55 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//http://www.spoj.com/problems/PRIME1/
+public class spoj_PRIME1_eratos {
+	static int testcase;
+	static int from;
+	static int to;
+	static int isPrime[];
+	static int time = 100_000_000; // 10억
+
+	static ArrayList<Integer> primeMemo;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		testcase = sc.nextInt();
+		setPrime(time);
+
+		for (int i = 0; i < testcase; i++) {
+			from = sc.nextInt();
+			to = sc.nextInt();
+			
+
+			getPrime(from, to);
+		}
+	}
+
+	// ex. 1~num까지의 소수 찾아 기입.
+	public static void setPrime(int num) {
+		primeMemo = new ArrayList<Integer>();
+		primeMemo.add(2);
+
+		for (int i = 2; i <= num; i++) {
+			for (int j = 0; j<primeMemo.size(); j++) {
+				if (i % primeMemo.get(j) == 0) {
+					break;
+				}
+				if(j+1==primeMemo.size()){
+					primeMemo.add(i);
+				}
+			}
+		}
+	}
+
+	public static void getPrime(int from, int to) {
+		for (Integer result : primeMemo) {
+			if(result>=from && result<=to){
+				System.out.println(result);
+			}
+		}
+
+
+	}
+}
