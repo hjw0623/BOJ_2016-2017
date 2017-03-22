d5c2bfd982bf4e8071dff46eb512efa839852a30
hjw0623 <hwj0623@naver.com>
2017-03-22 12:33:50 +0900

    BOJ#1520
    내리막길 https://www.acmicpc.net/problem/1520
    (1,1) 혹은 (0,0)에서 (N,M) (또는 N-1, M-1)로 가는 내리막 길 개수 구하기
    
    D[i][j] = i,j에서 시작하여 N,M으로 가는 내리막길의 개수
    
    method를 통해 x(세로), y(가로) 인덱스 좌표값을 넘겨받아 top down 방식으로 전개
    int solve(int x, int y){
       if(x==n-1 &&y==m-1) return 1; // 도착지점 도착하였으므로 내리막길 추가
       if(dp[x][y]!=0) return dp[x][y]; //memoization
     for(int i=0; i<4; i++){
       int next_x = x+nx[i]; int next_y = y+ny[i];
       if(범위 체크){
            if(내리막길) dp[x][y] +=go(next_x, next_y);
         }
       return dp[x][y];
    }

---
+++ DP_1520.java
@@ -0,0 +1,44 @@
+package algorithm_intermid;
+
+import java.util.Scanner;
+
+public class DP_1520 {
+	static int n,m; //n은 가로, m은 세로
+	static int dp[][];
+	static int map[][];
+	static int nx[] = {0,0,-1,1};
+	static int ny[] = {-1,1,0,0};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		m = sc.nextInt(); //세로 4
+		n = sc.nextInt(); //가로 5
+		map = new int [m][n];
+		dp = new int [m][n];
+		
+		for(int i=0; i<m; i++){
+			for(int j=0; j<n; j++){
+				map[i][j] = sc.nextInt();
+			}
+		}
+		for(int i=0; i<m; i++){
+			for(int j=0; j<n; j++){
+			}
+		}
+		System.out.println(go(0,0));
+	}
+	public static int go(int x, int y){
+		if(x==m-1 && y== n-1)return 1;
+		if(dp[x][y]>0)return dp[x][y];
+		for(int i=0; i<4; i++ ){
+			int next_x = x+nx[i];
+			int next_y = y+ny[i];
+			if(next_x < m && next_x >=0 && next_y < n &&next_y >= 0 ){
+				if(map[x][y]>map[next_x][next_y]){
+					dp[x][y] += go(next_x, next_y);
+				}
+			}
+		}
+		return dp[x][y];
+	}
+
+}
