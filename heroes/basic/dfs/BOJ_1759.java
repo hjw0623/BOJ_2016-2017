b2b2859571937393576ac75360130ca94aa76de3
hwj0623 <hwj0623@naver.com>
2017-09-29 20:36:25 +0900

    1759 백트래킹 문제

--- heroes/basic/dfs/BOJ_1759.java
+++ heroes/basic/dfs/BOJ_1759.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
@@ -12,8 +13,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_1759 {
 	static char moum[] = { 'a', 'e', 'i', 'o', 'u' };
-	static ArrayList<Character> m1, j1;
-	static ArrayList<String> dp;
+	
 	static int L, C;
 
 	public static void main(String[] args) throws IOException {
@@ -21,36 +21,45 @@ public class BOJ_1759 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		m1 = new ArrayList<Character>();
-		j1 = new ArrayList<Character>();
-		dp  = new ArrayList<String>();
+		String[] alpha = new String[C];
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < C; i++) {
 			char tmp = st.nextToken().charAt(0);
-			for (int k = 0; k < moum.length; k++) {
-				if (moum[k] == tmp)
-					m1.add(tmp);
-				else
-					j1.add(tmp);
-			}
+			
+			alpha[i] = ""+tmp;
 		}
-//		System.out.println(dp.size());
-//		Collections.sort(dp);
-//		for(int i=1; i<dp.size()-1; i++){
-//			System.out.println(dp.get(0));
-//			if(dp.get(i-1).equals(dp.get(i))){
-//				continue;
-//			}else{
-//				System.out.println(dp.get(i));
-//			}
-//		}
+		
+		Arrays.sort(alpha);
+		dfs(L, alpha, "", 0);
 	}
 
 	public static void dfs(int len, String alpha[], String pw, int index) {
 		
-
+		if(len ==pw.length()){
+			if(check(pw)){
+				System.out.println(pw);
+			}
+			return;
+		}
+		if(index >=alpha.length)
+			return;
+		
+		dfs(len,alpha,pw+alpha[index], index+1);
+		dfs(len, alpha, pw, index+1);
 	}
-
 	
-
+	public static boolean check(String str){
+		int ja = 0;
+		int mo = 0;
+		for (char x : str.toCharArray()) {
+			for(int i=0; i<moum.length; i++){
+				if(x == moum[i])
+					mo+=1;
+				else
+					ja+=1;
+			}
+		}
+		return ja >= 2 && mo >= 1;
+	}
 }
